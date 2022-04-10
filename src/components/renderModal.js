bc5316eb6de6a677051decfe6f6d6e7342d9a5ab
tonnyhawk <tonnyhawk93@gmail.com>
2022-04-10 18:55:18 +0300

    stage 7 перенес render modal refactoring

--- src/components/renderModal.js
+++ src/components/renderModal.js
@@ -1,10 +1,9 @@
-const modal = document.querySelector('#modal');
-const modalTitle = modal.querySelector('.modal-title');
-const modalDescription = modal.querySelector('.text-break');
-const modalLink = modal.querySelector('.full-article');
-const closeButton = modal.querySelector('.btn-secondary');
-
 const renderModal = ({ title, description, link }, i18) => {
+  const modal = document.querySelector('#modal');
+  const modalTitle = modal.querySelector('.modal-title');
+  const modalDescription = modal.querySelector('.text-break');
+  const modalLink = modal.querySelector('.full-article');
+  const closeButton = modal.querySelector('.btn-secondary');
   modalTitle.textContent = title;
   modalDescription.textContent = description;
   modalLink.href = link;
